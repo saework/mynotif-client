d804c789a4a98709a902b396192186200be3deeb
saework <saework@ya.ru>
2021-01-04 11:27:20 +0500

    Настройка проекта

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+// Prettier - CTRL + SHIFT + P - Format Document
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div>!!!</div>
     </div>
   );
 }
